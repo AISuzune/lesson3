016d25786026d5a0c57501861bb8234c6a025692
LKer <414662465@qq.com>
2022-10-29 14:10:24 +0800

    Initial commit

---
+++ lesson_3/lesson_3.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//	func main() {
+//		go repeater()
+//		go repeater()
+//		repeater()
+//	}
+//
+//	func repeater() {
+//		for {
+//			time.Sleep(time.Second)
+//			println("over.")
+//		}
+//	}
+
+//func main() {
+//	go learnFrontend()
+//	go learnAndroid()
+//	go learnMachineLearning()
+//	learnBacked()
+//}
+//
+//func learnBacked() {
+//	time.Sleep(10 * time.Minute)
+//	println("会了！")
+//}
+//
+//func learnAndroid() {
+//	time.Sleep(20 * time.Minute)
+//	println("悔了！")
+//}
+//
+//func learnFrontend() {
+//	time.Sleep(time.Nanosecond)
+//	fmt.Println("会了！")
+//}
+//
+//func learnMachineLearning() {
+//	time.Sleep(114514 * time.Minute)
+//	fmt.Println("废了！")
+//}
+
+//type Item struct {
+//	Name  string
+//	Count int
+//}
+//
+//func main() {
+//	pipeline := make(chan Item, 10)
+//	go func() {
+//		for {
+//			time.Sleep(1 * time.Second)
+//			pipeline <- Item{
+//				Name:  "螺丝",
+//				Count: 5,
+//			}
+//		}
+//	}()
+//	go func() {
+//		for {
+//			time.Sleep(2 * time.Second)
+//			pipeline <- Item{
+//				Name:  "齿轮",
+//				Count: 3,
+//			}
+//		}
+//	}()
+//	for {
+//		item := <-pipeline
+//		fmt.Printf("%#v\n", item)
+//	}
+//}
+
+//func main() {
+//	go 打电动()
+//	好康的 := 欢迎来我家玩()
+//	fmt.Println(好康的)
+//}
+//
+//func 欢迎来我家玩() string {
+//	time.Sleep(5 * time.Second)
+//	return "登dua郎"
+//}
+//
+//func 打电动() {
+//	fmt.Println("输了啦，都是你害的")
+//}
+
+//func main() {
+//	var 杰哥答应的好康的 = make(chan string)
+//	go func() {
+//		杰哥答应的好康的 <- 欢迎来我家玩()
+//	}()
+//	打电动()
+//	好康的 := <-杰哥答应的好康的
+//	fmt.Println(好康的)
+//}
+//
+//func 欢迎来我家玩() string {
+//	time.Sleep(5 * time.Second)
+//	return "登dua郎"
+//}
+//
+//func 打电动() {
+//	fmt.Println("输了啦，都是你害的")
+//}
+
+//func main() {
+//	var (
+//		ch1 = make(chan struct{})
+//		ch2 = make(chan struct{})
+//		ch3 = make(chan struct{})
+//	)
+//	go handleCh1(ch1)
+//	go handleCh1(ch2)
+//	go handleCh1(ch3)
+//	for {
+//		select {
+//		case _ = <-ch1:
+//			fmt.Println("get from ch1")
+//		case _ = <-ch2:
+//			fmt.Println("get from ch2")
+//		case _ = <-ch3:
+//			fmt.Println("get from ch3")
+//		}
+//	}
+//}
+//
+//func handleCh1(ch1 chan struct{}) {
+//	for {
+//		time.Sleep(3 * time.Second)
+//		ch1 <- struct{}{}
+//	}
+//}
+//func handleCh2(ch2 chan struct{}) {
+//	for {
+//		time.Sleep(4 * time.Second)
+//		ch2 <- struct{}{}
+//	}
+//}
+//func handleCh3(ch3 chan struct{}) {
+//	for {
+//		time.Sleep(2 * time.Second)
+//		ch3 <- struct{}{}
+//	}
+//}
+
+func main() {
+	var (
+		ch1  = make(chan struct{})
+		stop = make(chan struct{})
+	)
+	go handleCh1(ch1)
+	go func() {
+		time.Sleep(10 * time.Second)
+		stop <- struct{}{}
+	}()
+LOOP:
+	for {
+		select {
+		case _ = <-ch1:
+			fmt.Println("get from ch1")
+		case _ = <-stop:
+			break LOOP
+		}
+	}
+}
+func handleCh1(ch1 chan struct{}) {
+	for {
+		time.Sleep(3 * time.Second)
+		ch1 <- struct{}{}
+	}
+}
